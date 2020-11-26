f6d5330fd68ec65f83e5ed8707e98f08325ca7fd
Esther Nampiina <nay.esther08@gmail.com>
2020-11-27 00:49:14 +0100

    adding updates to program

--- signup/Signup/Program.cs
+++ signup/Signup/Program.cs
@@ -8,11 +8,27 @@ namespace Signup
         {
             int userAge;
             Console.WriteLine("Please enter your age: ");
-            int userAge = Convert.ToInt32(Console.ReadLine());
+            userAge = Convert.ToInt32(Console.ReadLine());
             if (userAge < 0 || userAge > 100)
             {
-                Console.WriteLine
+                Console.WriteLine("Invalid age. ");
+                Console.WriteLine("Age must be between 0 and 100. ");
+
+            }
+            else if (userAge < 18)
+                Console.WriteLine("Sorry you are underage");
+            else if (userAge < 20)
+                Console.WriteLine("You need parental guidance");
+            
+            else
+                {
+                    Console.WriteLine("Congratulations! ");
+                    Console.WriteLine("You may signup for the event. ");
+                    Console.ReadLine();
+                }
+
+
             }
         }
     }
-}
+
