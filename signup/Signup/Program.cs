e94981a3d00ce81d91ab971be27636c2df6369c6
Esther Nampiina <nay.esther08@gmail.com>
2020-11-26 23:28:37 +0100

    Adding signup folder

---
+++ signup/Signup/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Signup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+            Console.WriteLine("Please enter your age: ");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge < 0 || userAge > 100)
+            {
+                Console.WriteLine
+            }
+        }
+    }
+}
