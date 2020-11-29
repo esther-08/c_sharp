b2d76e767318c3f04e4a3149b5cfaa051515f1b8
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:43:44 +0100

    Adding updates to program

---
+++ giraffedowhile/giraffedowhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace giraffedowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+            }
+            while (index <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
