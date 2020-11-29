ddd3cedce2b190915d8391a57ab76a362170f107
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 19:04:34 +0100

    Adding static attributes

---
+++ giraffestaticattributes/giraffestaticattributes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace giraffestaticattributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song Kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+           
+            Console.WriteLine(Song.songCount);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ giraffestaticattributes/giraffestaticattributes/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace giraffestaticattributes
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount = 0;
+
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+        }
+    }
+}
