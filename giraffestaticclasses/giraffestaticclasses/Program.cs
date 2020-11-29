622254924620a28fb8732481d94fa8ee680d52a6
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 19:13:12 +0100

    Adding static classes

---
+++ giraffestaticclasses/giraffestaticclasses/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace giraffestaticclasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usefultools.SayHi("Essy");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ giraffestaticclasses/giraffestaticclasses/Usefultools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace giraffestaticclasses
+{
+    class Usefultools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
