3784465c603c6123adbddcbc4e078b8f20c76fb3
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 19:16:37 +0100

    Adding updates to program

---
+++ ifstatement/IfStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isMale = true;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+
+            }
+            else if(isMale && !isTall)
+            {
+                Console.WriteLine("You are a short man");
+            } 
+            else if (!isMale && isTall) 
+            {
+                Console.WriteLine("You are not a man but tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
