57c1cfc4f1e2540778024d8e7d76d77b1435ed29
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:42:03 +0100

    Adding updates to program

---
+++ giraffearray/giraffearray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace giraffearray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[,] numberGrid =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+
+                Console.WriteLine(numberGrid[2, 1]);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
