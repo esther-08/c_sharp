fe8b7cfe569fd7ecdf046800d82af811e09c6423
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:47:10 +0100

    Adding updates to program

---
+++ giraffeforloop/giraffeforloop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace giraffeforloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            
+            for (int i = 0; i <= luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
