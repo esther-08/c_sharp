327c2e50069ee8621acd9c24ed7fe0124d03a19d
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:46:22 +0100

    Adding updates to program

---
+++ giraffeexponent/giraffeexponent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace giraffeexponent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(3,4));
+            Console.ReadLine();
+        }
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+            for(int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+            return result;
+        }
+    }
+}
