5679e1b2798b145cf5c5d31227b1053440886b0b
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 19:02:14 +0100

    Adding objects

---
+++ giraffeobjects/giraffeobjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace giraffeobjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 800);
+            Book book3 = new Book();
+
+            Console.WriteLine(book2.author);
+
+            Console.ReadLine();
+        }
+    }
+}
