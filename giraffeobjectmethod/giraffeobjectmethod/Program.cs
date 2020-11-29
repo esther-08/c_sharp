86f991f625a9a23284c3895e713516637cecffc0
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:52:28 +0100

    Adding object

---
+++ giraffeobjectmethod/giraffeobjectmethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace giraffeobjectmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Jim", "Business", 4.2);
+            Student student2 = new Student("Ron", "Art", 3.1);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ giraffeobjectmethod/giraffeobjectmethod/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace giraffeobjectmethod
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student (string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
