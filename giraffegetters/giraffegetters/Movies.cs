5e2a0e94f6f0e4e6de1af997a4d08a9d86c0ab53
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:48:57 +0100

    Adding updates to program

---
+++ giraffegetters/giraffegetters/Movies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace giraffegetters
+{
+    class Movies
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movies(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if(value == "G" || value== "PG" || value == "PG-13" || value == "PG-18")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+
+        }
+    }
+}
---
+++ giraffegetters/giraffegetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace giraffegetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movies avengers = new Movies("The Avengers", "Joss Whedon", "PG-18");
+            Movies shrek = new Movies("Shrek", "Adam Admson", "PG-13");
+
+            Console.WriteLine(avengers.Rating);
+
+            Console.ReadLine ();
+        }
+    }
+}
