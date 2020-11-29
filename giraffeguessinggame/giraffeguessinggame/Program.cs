ac9b4ca810f22fa1c4ab0ff27124004d5de1482c
Esther Nampiina <nay.esther08@gmail.com>
2020-11-29 18:49:33 +0100

    Adding updates to program

---
+++ giraffeguessinggame/giraffeguessinggame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace giraffeguessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 4;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose! ");
+            }
+            else
+            {
+                Console.Write("You Win!");
+
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
